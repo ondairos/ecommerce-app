be4bf41f7d1e7ecd729caa62d9431e3c1bb6fff9
Ondairos <ondairos@gmail.com>
2023-05-24 13:59:08 +0300

    css grid

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,15 @@ export default async function Home() {
   const products: Product[] = await res.json();
 
   return (
-    <main className="mx-2">
+    <main className="min-h-screen max-w-7xl mx-auto px-8 xl:px-0 mt-48">
       <section className="flex flex-col space-y-12 pb-44">
         <h1 className="text-5xl font-bold text-center">Deals of the Day</h1>
 
-        {products.map((product) => {
-          return <Product key={product.id} product={product} />;
-        })}
+        <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          {products.map((product) => {
+            return <Product key={product.id} product={product} />;
+          })}
+        </div>
       </section>
     </main>
   );
